c7a00c09393828ecc30ecd6caffc699734829cee
4heart4 <58267793+4heart4@users.noreply.github.com>
2019-11-28 15:17:48 +0200

    Add files via upload

---
+++ HW_1.java
@@ -0,0 +1,14 @@
+public class HW_1 {
+    public static void main(String[] args){
+        byte q = 8;
+        short w = 16;
+        int e = 32;
+        long r = 64L;
+        float t = 23.45f;
+        double y = 234.34d;
+        System.out.println(q + w);
+        System.out.println(e - r);
+        System.out.println(t * y);
+        System.out.println(y / e);
+    }
+}
